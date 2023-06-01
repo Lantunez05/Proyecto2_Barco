8342d751be445dbf35925e5415052ea755c23fb8
Lantunez05 <126211785+Lantunez05@users.noreply.github.com>
2023-06-01 03:30:09 -0600

    Create pwm.h

---
+++ pwm.h
@@ -0,0 +1,17 @@
+/* 
+ * File:   pwm.h
+ * Author: Luis Antunez
+ *
+ * Created on 15 de mayo de 2023, 05:26 PM
+ */
+
+#ifndef pwm_H
+#define pwm_H 
+
+#include <xc.h>
+
+void PWM_Init(void);
+void PWM_dc(unsigned int duty_cycle);
+void PWM_manual(int adc_value);
+
+#endif
